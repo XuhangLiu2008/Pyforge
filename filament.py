f4c5475152310c9820c6ab0682425874e96a7066
XuhangLiu2008 <2451233667@qq.com>
2025-11-12 19:41:12 +0800

    1919810

--- filament.py
+++ filament.py
@@ -4,8 +4,16 @@ import numpy as np
 class Filament:
 
     def __init__(self):
+        
+        self.brand = ''
+        self.name = ''
 
         self.colour = np.zeros(3, dtype = np.uint8)
 
         self.refrative_index = np.zeros(3)
         self.extinction_coefficient = np.zeros(3)
+
+    def calculateCoefficients(self, samples):
+        # samples is a list of [thickness, colour]
+        # colour should be np.uint8 array with size 3
+        pass
